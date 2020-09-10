1a3d909e5186b20ee7b1a73c39b28db2a9bd389d
Anton Miroshnyk <miroshniktyt@gmail.com>
2020-09-10 21:51:14 +0300

    Initial Commit

---
+++ coctails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coctails
+//
+//  Created by Macbook Air on 10.09.2020.
+//  Copyright © 2020 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
